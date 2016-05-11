06cb752ebb1631cbae2ef12ba887a735011a6dd8
jmavila <jm.avila@bi4group.com>
2016-05-11 23:21:10 +0200

    install script

--- script/install.sh
+++ script/install.sh
@@ -12,9 +12,5 @@ source tdd_venv/bin/activate
 
 pip install -r tdd-kata/requirements.txt
 
-#sudo npm install gulp -g
-#sudo npm install gulp-shell
-
 #$1 should be the team number
-git branch team_%1
-git push --set-upstream origin team_$1
+git dheckout -b team_%1
